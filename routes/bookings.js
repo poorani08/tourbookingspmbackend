dcfb8e61b6b1f6dc6f1f51389f831f147ea0eef1
poorani08 <123159672+poorani08@users.noreply.github.com>
2025-04-23 10:58:51 +0530

    Update bookings.js

--- routes/bookings.js
+++ routes/bookings.js
@@ -1,6 +1,6 @@
 import express from "express";
 import {verifyUser,verifyAdmin} from '../utils/verifyToken.js';
-import { createBooking, getBooking, getallBooking } from "../controllers/bookingcontroller.js";
+import { createBooking, getBooking, getallBooking } from "../controllers/bookingController.js";
 const router=express.Router();
 router.post("/",verifyUser,createBooking)
 router.get("/:id",verifyUser,getBooking)
